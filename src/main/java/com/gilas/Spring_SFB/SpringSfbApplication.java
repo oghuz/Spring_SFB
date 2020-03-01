3f6b4238fcfceafec5128306fa9fcb6e349ce50d
osmanjan <hizmatla@gmail.com>
2020-03-01 12:49:57 -0600

    first commit

---
+++ src/main/java/com/gilas/Spring_SFB/SpringSfbApplication.java
@@ -0,0 +1,13 @@
+package com.gilas.Spring_SFB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSfbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSfbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gilas/Spring_SFB/SpringSfbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gilas.Spring_SFB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSfbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
